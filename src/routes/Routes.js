1468be53893290a2b2af4dcc1ac2b10c8d1619e6
salmanyagaka <salmanyagaka@gmail.com>
2020-04-28 23:06:37 +0300

    Added fetch stories functionality and assets

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,14 +2,14 @@
 import * as React from 'react';
 // third party packages
 import { Redirect, Route, Switch } from 'react-router-dom';
-import StoriesComponent from '../components/StoriesComponent'
+import StoriesContainer from '../containers/StoriesContainer'
 import HomeContainer from '../containers/HomeContainer'
 
 const Routes = () => (
   <Switch>
 
     <Route exact path="/" component={HomeContainer} />
-    <Route exact path="/stories" component={StoriesComponent} />
+    <Route exact path="/stories/:id" component={StoriesContainer} />
     {/* <Redirect to="/404" /> */}
   </Switch>
 );
