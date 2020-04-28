479e41d676fb3b9aa8daa6526af5d3995f0dfdd1
salmanyagaka <salmanyagaka@gmail.com>
2020-04-28 21:33:14 +0300

    Added fetch request and assets

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../assets/styles/navbar.css';
+
+const NavbarComponent = props => {
+  return (
+    <div>
+      <div className='topnav'>
+        <a className='active' href='#home'>
+          Home
+        </a>
+        <a href='#news'>News</a>
+        <a href='#contact'>Contact</a>
+        <a href='#about'>About</a>
+      </div>
+    </div>
+  )
+}
+
+export default NavbarComponent
